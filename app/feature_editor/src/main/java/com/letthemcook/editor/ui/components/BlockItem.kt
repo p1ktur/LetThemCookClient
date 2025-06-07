1e51f53b85bb74a08909793db13398520cd06b45
Renat Kovalov <renatkovalovofficial@gmail.com>
2025-06-07 18:46:22 +0300

    Fix

--- app/feature_editor/src/main/java/com/letthemcook/editor/ui/components/BlockItem.kt
+++ app/feature_editor/src/main/java/com/letthemcook/editor/ui/components/BlockItem.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Edit
-import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
